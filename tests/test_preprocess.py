8673d37d090feb3b2f73806b56685e2e00d05263
Kinshuk Vasisht <kinshukvasishtsmart@gmail.com>
2023-01-06 00:54:05 +0530

    Implement tests for modules using `pytest`

---
+++ tests/test_preprocess.py
@@ -0,0 +1,155 @@
+"""
+Test suite for preprocessing stage of the pipeline.
+"""
+
+import os
+import argparse
+
+import pytest
+
+from src.pipeline import preprocess
+
+@pytest.fixture(scope="session")
+# pylint: disable-next=redefined-outer-name,missing-function-docstring
+def prog():
+    return "test"
+@pytest.fixture(scope="session")
+# pylint: disable-next=redefined-outer-name,missing-function-docstring
+def args():
+    return argparse.Namespace(
+        courts=[ "SC" ], extractors=[ "generic" ],
+        output_dir=os.path.join("tests", "data"),
+        document_dir="judgments", debug=False
+    )
+
+@pytest.fixture(scope="session")
+# pylint: disable-next=redefined-outer-name,missing-function-docstring
+def data_indexes(prog, args):
+    return preprocess.load_indexes(prog, args)
+
+@pytest.fixture(scope="session")
+# pylint: disable-next=redefined-outer-name,missing-function-docstring
+def file_index(data_indexes):
+    return data_indexes[0]
+@pytest.fixture(scope="session")
+# pylint: disable-next=redefined-outer-name,missing-function-docstring
+def judgment_index(data_indexes):
+    return data_indexes[1]
+
+judgment_index_test_data = [
+    (
+        ({
+            'case_number': 'SC 188/2022',
+            # pylint: disable-next=line-too-long
+            'document_href': 'http://dhcappl.nic.in:8080/FreeText/download.do?FILENAME=dhc/PMS/judgement/12-10-2022//PMS10102022SC1882022_124159.pdf&ID=1921559389_1'
+        }, "SC"), {
+            'json': os.path.join("tests", "data", "json", "SC Judgments", "judgments SC sample page 1.json"),
+            'index': 0
+        }
+    ),
+    (
+        ({
+            'case_number': 'SC 316/2021',
+            # pylint: disable-next=line-too-long
+            'document_href': 'http://dhcappl.nic.in:8080/FreeText/download.do?FILENAME=dhc/PMS/judgement/04-10-2022//PMS27092022SC3162021_150037.pdf&ID=1921559389_5'
+        }, "SC"), {
+            'json': os.path.join("tests", "data", "json", "SC Judgments", "judgments SC sample page 1.json"),
+            'index': 1
+        }
+    ),
+    (
+        ({
+            'case_number': 'SC 420/1969',
+            # pylint: disable-next=line-too-long
+            'document_href': 'http://dhcappl.nic.in:8080/FreeText/download.do?FILENAME=dhc/PMS/judgement/04-10-2022//PMS27092022SC3162021_156937.pdf&ID=1921559389_5'
+        }, "SC"), None
+    )
+]
+
+@pytest.mark.parametrize("judgment_key,meta", judgment_index_test_data)
+# pylint: disable-next=redefined-outer-name,missing-function-docstring
+def test_judgment_index(judgment_index, judgment_key, meta):
+    assert len(judgment_index.data['SC']['data']) == 2
+
+    judgment, group = judgment_key
+    for search_key in judgment:
+        _judgment = { search_key: judgment[search_key] }
+        _result, _meta = judgment_index.get(_judgment, group)
+        assert _result is None or _result[search_key] == judgment[search_key]
+        assert _meta == meta
+
+file_index_test_data = [
+    (
+        (
+            os.path.join("tests", "data", "judgments", "SC Judgments", "1921559389_1.pdf"),
+            "SC Judgments"
+        ),
+        {
+            'json': os.path.join("tests", "data", "json", "SC Judgments", "judgments SC sample page 1.json"),
+            'index': 0
+        }
+    ),
+    (
+        (
+            os.path.join(
+                "tests", "data", "judgments", "SC Judgments",
+                "extracted_generic", "1921559389_1.txt"
+            ),
+            "extracted_generic"
+        ),
+        {
+            'json': os.path.join("tests", "data", "json", "SC Judgments", "judgments SC sample page 1.json"),
+            'index': 0
+        }
+    ),
+    (
+        (
+            os.path.join("tests", "data", "judgments", "SC Judgments", "1921559389_5.pdf"),
+            "SC Judgments"
+        ),
+        {
+            'json': os.path.join("tests", "data", "json", "SC Judgments", "judgments SC sample page 1.json"),
+            'index': 1
+        }
+    ),
+    (
+        (
+            os.path.join(
+                "tests", "data", "judgments", "SC Judgments",
+                "extracted_generic", "1921559389_5.txt"
+            ),
+            "extracted_generic"
+        ),
+        {
+            'json': os.path.join("tests", "data", "json", "SC Judgments", "judgments SC sample page 1.json"),
+            'index': 1
+        }
+    ),
+    (
+        (
+            os.path.join("tests", "data", "judgments", "SC Judgments", "1921559389_6.pdf"),
+            "SC Judgments"
+        ),
+        None
+    ),
+    (
+        (
+            os.path.join(
+                "tests", "data", "judgments", "SC Judgments",
+                "extracted_generic", "1921559389_6.txt"
+            ),
+            "extracted_generic"
+        ),
+        None
+    ),
+]
+
+@pytest.mark.parametrize("file_key,meta", file_index_test_data)
+# pylint: disable-next=redefined-outer-name,missing-function-docstring
+def test_file_index(file_index, file_key, meta):
+    assert len(file_index.data['SC Judgments']['data']) == 2
+    assert len(file_index.data['extracted_generic']['data']) == 2
+
+    filepath, group = file_key
+    _meta = file_index.get(filepath, group)
+    assert _meta == meta
---
+++ tests/test_utils.py
@@ -0,0 +1,70 @@
+"""
+Test suite for utility functions.
+"""
+
+import collections
+
+# import pytest
+
+from src import utils
+
+# pylint: disable-next=redefined-outer-name,missing-function-docstring
+def test_merge_dicts():
+    dict_a = collections.defaultdict(list)
+    dict_b = collections.defaultdict(list)
+
+    dict_a.update({ 'a': [1,2], 'b': [3] })
+    dict_b.update({ 'a': [3], 'c': [1,2,3] })
+
+    dict_c = utils.merge_dicts(dict_a, dict_b)
+    assert isinstance(dict_c, collections.defaultdict)
+    assert dict_c == { 'a': [ 1,2,3 ], 'b': [ 3 ], 'c': [ 1,2,3 ] }
+
+# pylint: disable-next=redefined-outer-name,missing-function-docstring
+def preprocess_1():
+    return []
+# pylint: disable-next=redefined-outer-name,missing-function-docstring
+def preprocess_2():
+    return {}
+# pylint: disable-next=redefined-outer-name,missing-function-docstring
+def phase_1(_list, _dict, **_):
+    _list.append(1)
+    _dict['p1'] = 1
+    return [ len(_list) ]
+# pylint: disable-next=redefined-outer-name,missing-function-docstring
+def phase_2(_lens, _list, _dict, **_):
+    _list.append(2)
+    _dict['p2'] = 2
+    return [ *_lens, len(_list) ]
+# pylint: disable-next=redefined-outer-name,missing-function-docstring
+def phase_3(_lens, _list, _dict, **_):
+    _list.append(3)
+    _dict['p3'] = 3
+    return [ *_lens, len(_list) ]
+# pylint: disable-next=redefined-outer-name,missing-function-docstring
+def postprocess_1(_lens, _list, _dict, **__):
+    return len(_lens), len(_list)
+# pylint: disable-next=redefined-outer-name,missing-function-docstring
+def postprocess_2(_lens, _list, _dict, **__):
+    return len(_lens), list(_dict.keys())
+
+# pylint: disable-next=redefined-outer-name,missing-function-docstring
+def test_pipeline():
+    pipeline = utils.Pipeline(
+        preprocessing={ '_list': preprocess_1, '_dict': preprocess_2 },
+        phases=[ phase_1, phase_2, phase_3 ],
+        postprocessing={ '_post1': postprocess_1, '_post2': postprocess_2  }
+    )
+
+    # pylint: disable-next=unbalanced-tuple-unpacking
+    pre_res, res, post_res = pipeline.execute(with_preprocessing_results=True)
+
+    assert pre_res  == {
+        '_list': [ 1, 2, 3 ],
+        '_dict': { 'p1': 1, 'p2': 2, 'p3': 3 }
+    }
+    assert res      == [ 1, 2, 3 ]
+    assert post_res == {
+        '_post1': (3, 3),
+        '_post2': (3, [ 'p1', 'p2', 'p3' ])
+    }
